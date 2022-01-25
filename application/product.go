a7cc7aedb74f3cd616a0728e411a015126d41ab4
Jonathan Henrique <jonathan.medeiros@picpay.com>
2022-01-25 17:23:02 -0300

    Creating product based on interface contract and testing implementation methods

---
+++ application/product.go
@@ -0,0 +1,85 @@
+package application
+
+import (
+	"errors"
+	"github.com/asaskevich/govalidator"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+type ProductInterface interface {
+	IsValid() (bool, error)
+	Enable() error
+	Disable() error
+	GetID() string
+	GetName() string
+	GetStatus() string
+	GetPrice() float64
+}
+
+const (
+	DISABLED = "disabled"
+	ENABLED  = "enabled"
+)
+
+type Product struct {
+	ID     string  `valid:"uuidv4"`
+	Name   string  `valid:"required"`
+	Price  float64 `valid:"float,optional"`
+	Status string  `valid:"required"`
+}
+
+func (p *Product) IsValid() (bool, error) {
+	if p.Status == "" {
+		p.Status = DISABLED
+	}
+
+	if p.Status != ENABLED && p.Status != DISABLED {
+		return false, errors.New("the status must be enable or disabled")
+	}
+
+	if p.Price < 0 {
+		return false, errors.New("the proce must be greater or equal to zero")
+	}
+
+	_, error := govalidator.ValidateStruct(p)
+	if error != nil {
+		return false, error
+	}
+
+	return true, nil
+}
+
+func (p *Product) Enable() error {
+	if p.Price > 0 {
+		p.Status = ENABLED
+		return nil
+	}
+	return errors.New("the price must be greater than zero to enable the product")
+}
+
+func (p *Product) Disable() error {
+	if p.Price == 0 {
+		p.Status = DISABLED
+		return nil
+	}
+	return errors.New("the price must be zero in order to have the product disabled")
+}
+
+func (p *Product) GetID() string {
+	return p.ID
+}
+
+func (p *Product) GetName() string {
+	return p.Name
+}
+
+func (p *Product) GetStatus() string {
+	return p.Status
+}
+
+func (p *Product) GetPrice() float64 {
+	return p.Price
+}
---
+++ application/product_test.go
@@ -0,0 +1,69 @@
+package application_test
+
+import (
+	"testing"
+	"github.com/jonathanmdr/go-hexagonal/application"
+	"github.com/stretchr/testify/require"
+	uuid "github.com/satori/go.uuid"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	error := product.Enable()
+	require.Nil(t, error)
+
+	product.Price = 0
+
+	error = product.Enable()
+	require.Equal(t, "the price must be greater than zero to enable the product", error.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	error := product.Disable()
+	require.Nil(t, error)
+
+	product.Price = 10
+
+	error = product.Disable()
+	require.Equal(t, "the price must be zero in order to have the product disabled", error.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, error := product.IsValid()
+	require.Nil(t, error)
+
+	product.Status = "INVALID"
+	_, error = product.IsValid()
+	require.Equal(t, "the status must be enable or disabled", error.Error())
+
+	product.Status = application.ENABLED
+	_, error = product.IsValid()
+	require.Nil(t, error)
+
+	product.Price = -10
+	_, error = product.IsValid()
+	require.Equal(t, "the proce must be greater or equal to zero", error.Error())
+
+	product.ID = "asdfg"
+	_, error = product.IsValid()
+	require.Error(t, error)
+
+	product.Name = ""
+	_, error = product.IsValid()
+	require.Error(t, error)
+}
