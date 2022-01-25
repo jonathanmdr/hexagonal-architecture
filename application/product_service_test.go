44371e3384dc47aa0bcb10949cb69419dae6885a
Jonathan Henrique <jonathan.medeiros@picpay.com>
2022-01-25 19:22:29 -0300

    Added methods Create, Enable and Disable in product_service

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -24,3 +24,44 @@ func TestProductService_Get(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, product, result)
 }
+
+func TestProductService_Create(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	product := mock_application.NewMockProductInterface(controller)
+	persistence := mock_application.NewMockProductPersistenceInterface(controller)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Create("product", 10)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
+
+func TestProductService_Enable(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+
+	product := mock_application.NewMockProductInterface(controller)
+	product.EXPECT().Enable().Return(nil)
+	product.EXPECT().Disable().Return(nil)
+
+	persistence := mock_application.NewMockProductPersistenceInterface(controller)
+	persistence.EXPECT().Save(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Enable(product)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+
+	result, err = service.Disable(product)
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
