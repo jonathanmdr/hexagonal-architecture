d067eae42267fd3dd017883c30c44a668c471425
Jonathan Henrique <jonathan.medeiros@picpay.com>
2022-01-27 18:04:39 -0300

    Created POST route to persist product with http server

--- adapters/web/handler/product.go
+++ adapters/web/handler/product.go
@@ -2,10 +2,12 @@ package handler
 
 import (
 	"encoding/json"
+	"net/http"
+
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
+	"github.com/jonathanmdr/go-hexagonal/adapters/web/dto"
 	"github.com/jonathanmdr/go-hexagonal/application"
-	"net/http"
 )
 
 func MakeProductHandlers(router *mux.Router, n *negroni.Negroni, service application.ProductServiceInterface) {
@@ -14,6 +16,11 @@ func MakeProductHandlers(router *mux.Router, n *negroni.Negroni, service applica
 			negroni.Wrap(getProduct(service),
 		),
 	)).Methods("OPTIONS", "GET")
+	router.Handle("/products",
+		n.With(
+			negroni.Wrap(createProduct(service),
+		),
+	)).Methods("OPTIONS", "POST")
 }
 
 func getProduct(service application.ProductServiceInterface) http.Handler {
@@ -30,6 +37,33 @@ func getProduct(service application.ProductServiceInterface) http.Handler {
 			err = json.NewEncoder(response).Encode(product)
 			if err != nil {
 				response.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+		},
+	)
+}
+
+func createProduct(service application.ProductServiceInterface) http.Handler {
+	return http.HandlerFunc(
+		func(response http.ResponseWriter, request *http.Request) {
+			response.Header().Set("Content-Type", "application/json")
+			var productDto dto.Product
+			err := json.NewDecoder(request.Body).Decode(&productDto)
+			if err != nil {
+				response.WriteHeader(http.StatusInternalServerError)
+				response.Write(jsonError(err.Error()))
+				return
+			}
+			product, err := service.Create(productDto.Name, productDto.Price)
+			if err != nil {
+				response.WriteHeader(http.StatusInternalServerError)
+				response.Write(jsonError(err.Error()))
+				return
+			}
+			err = json.NewEncoder(response).Encode(product)
+			if err != nil {
+				response.WriteHeader(http.StatusInternalServerError)
+				response.Write(jsonError(err.Error()))
 				return
 			}
 		},
